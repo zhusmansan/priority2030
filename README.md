c12ca01c6e902ea35a3fc6f9da4ca26f6de9577f
zhusmansan <zhusmansan@gmail.com>
2023-05-16 11:13:14 +0300

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # priority2030
 Download and analysis for priority program
+
+## Launch
+
+You should install nodejs to run this
+
+```bash
+node downloader.js > info.json
+```
---
+++ downloader.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+// import fetch from 'node-fetch';
+
+const LIST_URL = "https://lk.priority2030.ru/api/v0/priority/list";
+
+async function getData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+async function getAllData() {
+  let list = await getData(LIST_URL);
+
+  for (let i = 0; i < 1/*list.data.participants.length*/; i++) {
+    let listItem = list.data.participants[i];
+    const detailsUrl = `https://lk.priority2030.ru/api/v0/priority/${listItem.id}/info`;
+    listItem.info = (await getData(detailsUrl)).data;
+  }
+  console.log(JSON.stringify(list));
+}
+
+getAllData();
